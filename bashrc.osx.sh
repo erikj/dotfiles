9e5d17ebabf98d44f968ee5c16b73177866b2223
Erik Johnson <ej@ucar.edu>
2012-02-27 10:46:45 -0700

    reverting alias m to textmate

--- bashrc.osx.sh
+++ bashrc.osx.sh
@@ -8,7 +8,7 @@ export SVN_EDITOR="mate -w"
 
 alias gmail='open https://mail.google.com/'
 alias weather="open http://www.wunderground.com/cgi-bin/findweather/getForecast?query=80301"
-alias m='subl'
+alias m='mate'
 alias s='subl'
 alias jsc='/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc'
 
